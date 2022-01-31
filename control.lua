a1183f30a4f1e694e28fb6f2c10a4bc95421b9cb
HARPOTHEGREAT <c.hayes.harrison@gmail.com>
2022-01-30 21:27:57 -0500

    Update control.lua

--- control.lua
+++ control.lua
@@ -12,7 +12,12 @@ script.on_event(defines.events.on_player_created, function(event)
     local player = game.get_player(event.player_index)
 
     local screen_element = player.gui.screen
-    local main_frame = screen_element.add{type="frame", name="ugg_main_frame", caption={"ugg.hello_world"}}
+    local main_frame = screen_element.add{type="frame", name="ugg_main_frame"}
     main_frame.style.size = {385, 165}
     main_frame.auto_center = true
+        
+    local content_frame = main_frame.add{type="frame", name="content_frame", direction="vertical", style="ugg_content_frame"}
+    local controls_flow = content_frame.add{type="flow", name="controls_flow", direction="horizontal", style="ugg_controls_flow"}
+
+    controls_flow.add{type="button", name="ugg_controls_toggle", caption={"ugg.deactivate"}}
 end)
