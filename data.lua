ef600c53b6ffd31dba3a352a75b72d41b0f01063
TFJamin <brh12@mindspring.com>
2022-01-30 21:40:23 -0500

    Update data.lua

--- data.lua
+++ data.lua
@@ -10,12 +10,12 @@ data:extend({
       type = 'custom-input',
       name = 'tas-tools:frame-advance',
       consuming = nil,
-      }
+      },
     
     {
       type = 'custom-input',
       name = 'tas-tools:toggle-input-display',
       consuming = nil,
-      }
+      },
     
     })
